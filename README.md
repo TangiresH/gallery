d50022cd758248c2d79701356e5e926ec93b0464
TangiresH <dimon4ik1010@gmail.com>
2023-03-17 02:05:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Gallery
+
+The interface is divided into two parts.
+On the left side, the user sees a list of albums.
+It is taken from https://jsonplaceholder.typicode.com/albums
+
+When the user clicks on an album on the right side he will see the photos from this album.
+These are taken from https://jsonplaceholder.typicode.com/photos?albumId=ID where instead of ID insert the ID of the album you need.
+
+Immediately after loading the application and getting the list of albums, on the right side
+shows the photos from the first album in the list.
---
+++ index.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const PHOTO_SELECTOR = ".photos";
+const ALBUM_LINKS_SELECTOR = "#containerForAlbumLinks";
+const ALBUM_LINK_SELECTOR = ".albumLink";
+const FIRST_ALBUM_INDEX = 0;
+const containerForPhotos = document.querySelector(PHOTO_SELECTOR);
+const containerForAlbums = document.querySelector(ALBUM_LINKS_SELECTOR);
+
+
+containerForAlbums.addEventListener("click", onContainerForAlbumsClick);
+
+init();
+
+
+function init() {
+    GalleryApi.getList()
+        .then((albums) => {
+            const albumId = albums[FIRST_ALBUM_INDEX].id;
+
+            renderAlbumLinks(albums);
+            getAlbumPhotos(albumId);
+        })
+        .catch(showError);
+}
+
+function renderAlbumLinks(albums) {
+    const html = albums.map(generateAlbumLinkHTML).join('');
+
+    containerForAlbums.innerHTML = html;
+}
+
+function generateAlbumLinkHTML(album) {
+    return `
+    <div class="albumLink" data-id="${album.id}">
+    <a href="#" id="link">${album.title}</a>
+    </div>`;
+}
+
+function onContainerForAlbumsClick(e) {
+    const albumLink = e.target.closest(ALBUM_LINK_SELECTOR);
+    let id = albumLink.dataset.id;
+
+    if (id) {
+        getAlbumPhotos(id);
+    }
+}
+
+function getAlbumPhotos(albumId) {
+    GalleryApi.getPhotos(albumId)
+        .then(renderPhotos)
+        .catch(showError);
+}
+
+function renderPhotos(photos) {
+    const html = photos.map(generatePhotoHTML).join('');
+
+    containerForPhotos.innerHTML = html;
+}
+
+function generatePhotoHTML(photo) {
+    return `<img src=${photo.thumbnailUrl}/>`
+}
+
+function showError(error) {
+    alert(error.message);
+}
